1e9f7971290fcb7f212aadacea1f98754453d28f
James Merrill <jamesmerrill613@gmail.com>
2016-10-09 00:05:14 +0000

    Lots of work to go......

--- main.cpp
+++ main.cpp
@@ -1,79 +1,108 @@
 #include <stdio.h>
-//#include <unistd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <chrono>
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 
+const int MAX_SIZE = 128;
 
 std::string GetInput()
 {
-	std::string input;
+ 	std::string input;
 	getline(std::cin, input);
 	return input;
 };
 
-void Parse(std::string input)
+void Parse(std::string input, char* &argc, char**&argv)
 {
-	std::vector<std::string> line;
-	int current = 0;
-	for (int i = 0; i < input.size(); i++)
+	argv = new char*[MAX_SIZE];
+	int last = 0;
+	int next = 0;
+	int argCount = 0;
+	while (next < input.length())
 	{
-		if (input[i] == ' ')
-			line.push_back();
-
-
+		next = input.find(' ', last);
+		if (next == -1)
+			next = input.length();
+		if (last < next)
+		{
+			char* word = new char[MAX_SIZE];
+			int j = 0;
+			for (int i = 0; i < next; i++)
+			{
+				word[j] = input[i];
+				j++;
+			}
+			if (argCount == 0)
+			{
+				argc = word;
+				argc[j] = NULL;
+			}
+			argv[argCount] = word;
+			argCount++;
+			last = next + 1;
+		}
 	}
+	argv[argCount] = NULL;
+	//return argv;
 };
 
-
-//void RunChild()
-//{
-//	execvp(....);
-//	perror();
-//	exit();
-//};
-//
-//void RunParent()
-//{
-//	waitpid(pid);
-//	auto stop = std::chrono::steady_clock::now();
-//	ptime += stop - start;
-//};
 
 
 int main()
 {
+	//Get INPUT
+	std::cout << "[cmd]: ";
+	std::string input = GetInput();
+	//Parse INPUT
+	char* argc = new char[MAX_SIZE];
+	char**argv = new char*[MAX_SIZE];
+	Parse(input, argc, argv);
 
-	while (true)
+//	while (true)
+//	{
+//		std::string input = GetInput();
+//		Parse(input);
+//	}
+
+
+/*	while (....)
 	{
-		std::string input = GetInput();
-		Parse(input);
-	}
 
+		//get input
+		//parse input
+		switch (command)
+		{
+*///		default:
+//      execvp(....);
+//  			auto start = std::chrono::steady_clock::now();
+			pid_t pid = fork();
 
-	//while (....)
-	//{
-	//	 
-	//	//get input
-	//	//parse input
-	//	switch (command)
-	//	{
-	//	default:
-	//		auto start = std::chrono::steady_clock::now();
-	//		auto pid = fork();
-	//		
-	//		if (pid == 0)
-	//			RunChild();
+			if (pid == 0)
+			{
+				execvp(argc, argv);
+				perror("execvp did not run");
+				std::cout << "this is the child process" << std::endl;
+				exit(0);
 
-	//		else if (pid > 0)
-	//			RunParent();
+			}
+			else if (pid > 0)
+			{
+				waitpid(pid, NULL, 0);
+				std::cout << "this is the parent process" << std::endl;
+//				auto stop = std::chrono::steady_clock::now();
+//				ptime += stop - start;
+			}
+//			else
+			//	perror(0); //fork failed
+//		}
+//	}
 
-	//		else
-	//			perror(); //fork failed
-	//	}
-	//}
-
+	std::cout << "main is running" << std::endl;
 	return 0;
 };
+
