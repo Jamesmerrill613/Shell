8cb9f03addfc4d8d3c9a710736ded4c08a213f53
James Merrill <jamesmerrill613@gmail.com>
2016-10-14 23:28:48 +0000

    Added New Parse Methods

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ public:
 	void show()
 	{
 		for (int i = list.size(); i > 0; i--)
-			std::cout << "   " << i << ") " << list[list.size() - i] << std::endl;
+			std::cout << "    " << i << ") " << list[list.size() - i] << std::endl;
 	};
 	std::string use(int i)
 	{
@@ -50,7 +50,70 @@ std::string GetInput()
 	return input;
 };
 
-void Parse(std::string input, std::vector<std::string>& argv)
+void Parse_Files(std::string& input, std::string& infile, std::string& outfile) 
+{
+	infile = "";
+	outfile = "";
+		int posIn = input.find("<");
+		int posOut = input.find(">");
+		if (posIn == -1 && posOut > -1)
+		{
+			outfile = input.substr(posOut + 1);
+			input = input.substr(0, posOut);
+		}	
+		else if (posIn > -1 && posOut == -1)
+		{
+			infile = input.substr(posIn + 1);
+			input = input.substr(0, posIn);
+		}
+		else if (posIn > -1 && posOut > -1)
+		{
+			if (posIn > posOut)
+			{
+				infile = input.substr(posIn + 1);
+				input = input.substr(0, posIn);
+				outfile = input.substr(posOut + 1);
+				input = input.substr(0, posOut);
+			}
+			else
+			{
+				outfile = input.substr(posOut + 1);
+				input = input.substr(0, posOut);
+				infile = input.substr(posIn + 1);
+				input = input.substr( 0, posIn);
+			
+			}
+
+		}
+}
+
+void Parse_Pipe(std::string input, std::vector<std::string>& cmds, std::string& infile, std::string& outfile)
+{
+	cmds.clear();
+	int pos = 1;
+	bool shouldContinue = true;
+	while (shouldContinue)
+	{
+		pos = input.find("|");
+		if (pos < 0)
+		{
+			pos = input.size();
+			Parse_Files(input, infile, outfile);
+			cmds.push_back(input);
+			shouldContinue = false;
+		}	
+		if (pos < input.size() && pos > 0)
+		{
+			cmds.push_back(input.substr(0, pos));
+			input = input.substr(pos + 1);
+		}
+		else if (pos == 0)
+			input = input.substr(pos + 1);
+	}
+}
+
+
+void Parse_Cmd(std::string input, std::vector<std::string>& argv)
 {
 	argv.clear();
 	int pos = -1;
@@ -85,34 +148,49 @@ int main()
 	while (true)
 	{
 
+
 		//GET INPUT
 		std::cout << "[cmd]: ";
 		std::string input = GetInput();
 		history.add(input);
 
+		//PARSE PIPE
+		std::vector<std::string> cmds;
+		std::string infile, outfile;
+		Parse_Pipe(input, cmds, infile, outfile);
+
+
+		for (int x = 0; x < cmds.size(); x++)
+		//RUN THE FOLLOWING FORK FOR EACH COMMAND IN cmds 
+		{
+
+
+
+
+
 		//PARSE INPUT
 		std::vector<std::string> args;
-		Parse(input, args);
+		Parse_Cmd(cmds[x], args);
 
 		//CHECK COMMANDS
 		bool shouldFork = true;
-		if (args[0] == "exit")
-			return 0;
-		if (args[0] == "ptime")
-		{
-			std::cout << "Time spent executing child process: " << ptime.count() << " microseconds" << std::endl;
-			shouldFork = false;
-		}
-		if (args[0] == "^")
-		{
-			history.remove();
-			Parse(history.use(std::stoi(args[1], nullptr, 10)), args);
-		}
-		if (args[0] == "history")
-		{
-			history.show();
-			shouldFork = false;
-		}
+//		if (args[0] == "exit")
+//			return 0;
+//		if (args[0] == "ptime")
+//		{
+//			std::cout << "Time spent executing child process: " << ptime.count() << " microseconds" << std::endl;
+//			shouldFork = false;
+//		}
+//		if (args[0] == "^")
+//		{
+//			history.remove();
+//			Parse_Cmd(history.use(std::stoi(args[1], nullptr, 10)), args);
+//		}
+//		if (args[0] == "history")
+//		{
+//			history.show();
+//			shouldFork = false;
+//		}
 
 		//FORK
 		if (shouldFork)
@@ -142,7 +220,8 @@ int main()
 			}
 			else
 				perror(0); //fork failed
-		}
+		}	
+	}
 	}
 	return 0; 
 }
